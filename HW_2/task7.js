e388b688ee15a6f597f522b0c1ab63a265b46254
Shanya7 <42109660+Shanya7@users.noreply.github.com>
2022-01-17 17:39:33 +0200

    Update task7.js
    task 7 is completed

--- HW_2/task7.js
+++ HW_2/task7.js
@@ -7,3 +7,50 @@
 В діапазоні годин 17-23 – має виводитися привітання “Доброго вечора”.
 
 */
+var today = new Date();
+var currentHour = today.getHours();
+
+//variant 1
+  switch(currentHour){
+  case 23:
+  case 00:
+  case 01:
+  case 02:
+  case 03:
+  case 04:
+  case 05:
+  console.log("Доброї ночі")
+  break;
+  case 06:
+  case 07:
+  case 08:
+  case 09:
+  case 10:
+  case 11:
+  console.log("Доброго ранку")
+  break;
+  case 12:
+  case 13:
+  case 14:
+  case 15:
+  case 16:
+  case 17:
+  console.log("Доброго дня")
+  break;
+
+  default:
+  console.log("Доброго вечора")
+  break;
+
+}
+
+//variant 2
+
+if(currentHour>=23 && currentHour <= 5)
+   console.log("Доброї ночі");
+else if (currentHour>=06 && currentHour<=11)
+  console.log("Доброго ранку");
+else if (currentHour>=12 && currentHour<=17) 
+  console.log("Доброго дня");
+else
+  console.log("Доброго вечора")
