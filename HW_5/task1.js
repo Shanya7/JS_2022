40187ba64c9b53462192317e7d25612259485c34
Shanya7 <anna.ku@gmail.com>
2022-01-26 09:01:15 +0200

    task 1 is updated

--- HW_5/task1.js
+++ HW_5/task1.js
@@ -19,3 +19,17 @@ let mentor = {
 };
 
 console.log(Object.keys(mentor).length)
+
+
+//
+function propsCount(currentObject){
+    
+    return (Object.keys(mentor).length);
+}
+let mentor = { 
+    course: "JS fundamental", 
+    duration: 3,
+    direction: "web-development" ,
+    
+};
+console.log(propsCount(mentor))
