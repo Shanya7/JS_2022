5d2c3ec24a53b809e7965a1e9153a8aa93a91262
Shanya7 <anna.ku@gmail.com>
2022-01-26 20:31:48 +0200

    task1 has been updated

--- HW_5/task1.js
+++ HW_5/task1.js
@@ -1,35 +1,25 @@
-/*1. Напишіть функцію propsCount(currentObject), яка приймає об’єкт і визначає кількість властивостей цього об’єкта.
-Наприклад для об’єкта
-let mentor = { 
-            course: "JS fundamental", 
-            duration: 3,
-            direction: "web-development" 
-        };
-Результат має бути 3.
-propsCount(mentor);  // 3
+function propsCount(currentObject) {
+  return Object.keys(mentor).length;
+}
+let mentor = {
+  course: "JS fundamental",
+  duration: 3,
+  direction: "web-development",
+};
+console.log(propsCount(mentor));
+
+//*********************2nd*************************
+/*
+function propsCount(currentObject) {
+  return currentObject;
+}
+let mentor = {
+  course: "JS fundamental",
+  duration: 3,
+  direction: "web-development",
+};
+
+console.log(Object.keys(mentor).length);
 */
-function propsCount(currentObject){
-    
-    return currentObject;
-}
-let mentor = { 
-    course: "JS fundamental", 
-    duration: 3,
-    direction: "web-development" ,
-};
-
-console.log(Object.keys(mentor).length)
 
 
-//
-function propsCount(currentObject){
-    
-    return (Object.keys(mentor).length);
-}
-let mentor = { 
-    course: "JS fundamental", 
-    duration: 3,
-    direction: "web-development" ,
-    
-};
-console.log(propsCount(mentor))
