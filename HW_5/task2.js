dd3f02f06e10528a161509bad07f5445430c8e59
Shanya7 <anna.ku@gmail.com>
2022-01-27 09:14:44 +0200

    task2 updated

--- HW_5/task2.js
+++ HW_5/task2.js
@@ -1,6 +1,3 @@
-/*2. Створіть довільний об’єкт, який має 5 полів. Необхідно написати функцію showProps(obj), яка приймає даний об’єкт і виводить
- список його властивостей записаних в масив. Виведіть також масив значень властивостей об’єкта.*/
-
 let myObj = {
   field1: "value1",
   field2: "value2",
@@ -12,17 +9,13 @@ let myArrayKeys = [];
 let myArrayValues = [];
 function showProps(obj) {
   obj = myObj;
-  //console.log(obj)
   for (let i in obj) {
-    //console.log(i)
     myArrayKeys.push(i);
     myArrayValues.push(myObj[i]);
   }
 
-  return myArrayKeys + myArrayValues;
-  // console.log(myArrayValues)
-  // console.log(myArrayKeys)
+  const result = [myArrayKeys, myArrayValues];
+  return ("List of properties: " + result[0] +"  List of property values: " +  result[1] );
 }
 
 console.log(showProps());
-//console.log(showProps(myArrayKeys));
