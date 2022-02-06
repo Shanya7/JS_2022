d580afceb438a90883342bfc59e0233ff322d061
Shanya7 <anna.ku@gmail.com>
2022-02-06 21:59:22 +0200

    task2

--- HW_8/task2.js
+++ HW_8/task2.js
@@ -1,12 +1,5 @@
-/*
- Напишіть функцію, яка приймає рядкові дані і виконує перевірку на їх відповідність емейлу. Валідними вважаються всі символи на різних позиціях.
-	Приклад роботи:
-checkEmail("Qmail2@gmail.com");
-true
-
-*/
 function checkEmail (value) {
-    let regExp=/\w+|\d+\S+/g;
+    let regExp=/.+\@.+\..+/g;
    let result = regExp.test(value);
    return result;
 }
@@ -14,9 +7,4 @@ console.log(checkEmail("Qmail2@gmail.com"))
 
 
 
-function checkEmail2 (value) {
-    let regExp=/\S+@\S+\.\S+/;
-   let result = regExp.test(value);
-   return result;
-}
-console.log(checkEmail2("Qmail2@gmail.com"))
+
